9bc2362451fc518d3601ecb2c4e68a42f867bd4c
Patrick Falk Nielsen <panie@jysk.com>
2023-12-14 14:47:19 +0100

    fix: convert to ico files

--- internal/tray/assets/assets.go
+++ internal/tray/assets/assets.go
@@ -2,29 +2,29 @@ package assets
 
 import _ "embed"
 
-//go:embed green-circle-icon.png
+//go:embed green-circle-icon.ico
 var StatusIconGreen []byte
 
-//go:embed red-circle-icon.png
+//go:embed red-circle-icon.ico
 var StatusIconRed []byte
 
-//go:embed icon.png
+//go:embed icon.ico
 var Icon []byte
 
-//go:embed icon-1.png
+//go:embed icon-1.ico
 var AnimateIcon1 []byte
 
-//go:embed icon-2.png
+//go:embed icon-2.ico
 var AnimateIcon2 []byte
 
-//go:embed icon-3.png
+//go:embed icon-3.ico
 var AnimateIcon3 []byte
 
-//go:embed icon-4.png
+//go:embed icon-4.ico
 var AnimateIcon4 []byte
 
-//go:embed icon-5.png
+//go:embed icon-5.ico
 var AnimateIcon5 []byte
 
-//go:embed icon-6.png
+//go:embed icon-6.ico
 var AnimateIcon6 []byte
