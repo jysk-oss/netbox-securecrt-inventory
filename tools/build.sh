a452fc5f0883d025c18838df52f92de8ed531049
Patrick Falk Nielsen <panie@jysk.com>
2023-12-14 09:19:06 +0100

    fix: zip mac app structure

--- tools/build.sh
+++ tools/build.sh
@@ -3,3 +3,4 @@ cp -r tools/assets/securecrt-inventory.app dist
 
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags "-H=windowsgui" -o dist/securecrt-inventory.exe main.go
 CGO_ENABLED=1 go build -o dist/securecrt-inventory.app/Contents/MacOS/securecrt-inventory main.go
+zip -r dist/securecrt-inventory.app.zip dist/securecrt-inventory.app 
