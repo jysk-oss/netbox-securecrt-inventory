e74b8c7bc6ffbee8599cf1f5661a00ce77c5f274
Patrick Falk Nielsen <panie@jysk.com>
2023-12-18 08:40:39 +0100

    Revert "fix: convert to ico files"
    This reverts commit 9bc2362451fc518d3601ecb2c4e68a42f867bd4c.

--- internal/tray/assets/assets.go
+++ internal/tray/assets/assets.go
@@ -2,29 +2,29 @@ package assets
 
 import _ "embed"
 
-//go:embed green-circle-icon.ico
+//go:embed green-circle-icon.png
 var StatusIconGreen []byte
 
-//go:embed red-circle-icon.ico
+//go:embed red-circle-icon.png
 var StatusIconRed []byte
 
-//go:embed icon.ico
+//go:embed icon.png
 var Icon []byte
 
-//go:embed icon-1.ico
+//go:embed icon-1.png
 var AnimateIcon1 []byte
 
-//go:embed icon-2.ico
+//go:embed icon-2.png
 var AnimateIcon2 []byte
 
-//go:embed icon-3.ico
+//go:embed icon-3.png
 var AnimateIcon3 []byte
 
-//go:embed icon-4.ico
+//go:embed icon-4.png
 var AnimateIcon4 []byte
 
-//go:embed icon-5.ico
+//go:embed icon-5.png
 var AnimateIcon5 []byte
 
-//go:embed icon-6.ico
+//go:embed icon-6.png
 var AnimateIcon6 []byte
